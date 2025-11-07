a591f39c89fcdc01683162c05fb4a8fa124e68fc
Dimes2212 <raduloffd@gmail.com>
2025-11-07 18:09:56 +0300

    Fix next config for GH Pages and redeploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
+// const repo = 'spa_products';
+// const isProd = process.env.NODE_ENV === 'production';
+// module.exports = {
+//   output: 'export',
+//   basePath: isProd ? `/${repo}` : '',
+//   assetPrefix: isProd ? `/${repo}/` : '',
+//   images: { unoptimized: true },
+// };
+
+import type { NextConfig } from 'next';
+
 const repo = 'spa_products';
 const isProd = process.env.NODE_ENV === 'production';
-module.exports = {
+
+const nextConfig: NextConfig = {
   output: 'export',
+  images: { unoptimized: true },
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
-  images: { unoptimized: true },
+  // опционально:
+  // trailingSlash: true,
 };
+
+export default nextConfig;
