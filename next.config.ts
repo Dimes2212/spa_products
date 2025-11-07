dc9461edcb65687407d510558e54113bda7c64b5
Dimes2212 <raduloffd@gmail.com>
2025-11-07 18:35:37 +0300

    ..

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from 'next';
-
 const repo = 'spa_products';
 const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
-  // trailingSlash: true, // опционально
 };
-
 export default nextConfig;
